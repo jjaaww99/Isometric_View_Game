33c7011ab500a285ce3f7364d3a151d789a7bbe1
Seol497 <dydrjs4974@gmail.com>
2024-05-28 17:55:42 +0900

    player stats 구현

--- Assets/KYG/UI Manager.cs
+++ Assets/KYG/UI Manager.cs
@@ -1,28 +1,10 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 using UnityEngine.UI;
 
 
 public class UIManager : MonoBehaviour
 {
-    [Header("¿ÃπÃ¡ˆ")]
-    [SerializeField]
-    private Slider expSlider;
-    [SerializeField]
-    private Image hp_bar;
-    [SerializeField]
-    private Image stamina_bar;
-
-    private void Awake()
-    {
-        hp_bar = GetComponent<Image>();
-    }
-    private void Start()
-    {
-        
-    }
-    private void Update()
-    {
-        
-    }
 
 }
