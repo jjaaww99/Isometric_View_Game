6129318d23580594dab30614bee228c2cd37a0cd
Xion079 <rhdudals98@gmail.com>
2024-06-12 17:32:33 +0900

    몬스터별 공격상태 변화 프레임 추가, 몬스터 상태머신 리팩토링

---
+++ Assets/Monster/Scripts/MonsterState/ZombieAttackState.cs
@@ -0,0 +1,23 @@
+﻿public class ZombieAttackState : MonsterAttackState
+{
+    public override void EnterState(MonsterStateManager monster)
+    {
+        base.EnterState(monster);
+
+
+    }
+
+    public override void UpdateState(MonsterStateManager monster)
+    {
+        base.UpdateState(monster);
+
+
+    }
+
+    public override void ExitState(MonsterStateManager monster)
+    {
+        base.ExitState(monster);
+
+
+    }
+}
